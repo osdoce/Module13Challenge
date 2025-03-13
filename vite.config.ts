136ff24c2307e0bc61d94b20808109c36a293381
Oscar Ram <166548135+osdoce@users.noreply.github.com>
2025-03-13 00:41:39 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-//  envDir: './env',
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT) || 3000,
+    host: '0.0.0.0', // Permite que Render lo detecte
+  },
+  preview: {
+    port: parseInt(process.env.PORT) || 3000,
+    host: '0.0.0.0',
+    allowedHosts: ['module13challengecandidatesearchapp.onrender.com'], // Agrega tu dominio aquí
+  }
 });
